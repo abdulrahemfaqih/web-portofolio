fcf0ea9970069589dc0ee360b81770785a09349e
abdulrahemfaqih <faqih3935@gmail.com>
2025-01-31 14:49:23 +0700

    fix: dynamic component

---
+++ src/articles/index.js
@@ -0,0 +1,7 @@
+import implementasi_repository_servive_laravel_11 from "./implementasi-service-repository-pattern-laravel-11";
+
+
+export const articleMapping = {
+    'implementasi-service-repository-pattern-laravel-11': implementasi_repository_servive_laravel_11,
+
+};
--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,19 +1,21 @@
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import { articleMapping } from '../articles';
 
 const ArticleDetail = () => {
     const { flag } = useParams();
     const location = useLocation();
     const date = location.state?.date;
 
-    // Lazy load komponen artikel berdasarkan flag
-    const ArticleComponent = lazy(() => import(`../articles/${flag}`));
+    const ArticleComponent = articleMapping[flag];
+
+    if (!ArticleComponent) {
+        return <Navigate to="/" replace />;
+    }
 
     return (
         <div className="container max-w-4xl mx-auto px-4">
-            <Suspense fallback={<div>Memuat artikel...</div>}>
-                <ArticleComponent date={date} />
-            </Suspense>
+            <ArticleComponent date={date} />
         </div>
     );
 };
